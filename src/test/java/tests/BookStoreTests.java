1cc04b6436432403e94ef8f201d81811099614bb
kylosova_iuliia <sirintegral1991@gmail.com>
2021-05-27 22:50:50 +0300

    Added tests with diff types of log() + post

--- src/test/java/tests/BookStoreTests.java
+++ src/test/java/tests/BookStoreTests.java
@@ -1,10 +1,15 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static filters.CustomLogFilter.customLogFilter;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
 
 public class BookStoreTests {
     @Test
@@ -36,5 +41,52 @@ public class BookStoreTests {
                 .log().body()
                 .body("books", hasSize(greaterThan(0)));
 
+    }
+
+    @Test
+    void generateTokenTest() {
+        Map<String, String> data = new HashMap<>();
+        data.put("userName","alex");
+        data.put("password","asdsad#frew_DFS2");
+        given()
+                .contentType(JSON)
+                .body(data.toString())
+                .when()
+                .log().uri()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
+    }
+
+    @Test
+    void withAllureListenerTest() {
+        given()
+                .filter(new AllureRestAssured())
+                .contentType(JSON)
+                .body("{ \"userName\": \"alex\", \"password\": \"asdsad#frew_DFS2\" }")
+                .when()
+                .log().uri()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
+    }
+
+    @Test
+    void withFilterCustomTest() {
+        given()
+                .filter(customLogFilter().withCustomTemplates())
+                .contentType(JSON)
+                .body("{ \"userName\": \"alex\", \"password\": \"asdsad#frew_DFS2\" }")
+                .when()
+                .log().uri()
+                .post("https://demoqa.com/Account/v1/GenerateToken")
+                .then()
+                .log().body()
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."));
     }
 }
